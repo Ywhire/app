ca77ea253ade4503b437d9dff0d9008c108c5bb3
KubilayAydede <kubilayaydede1@hotmail.com>
2021-06-13 21:51:07 +0300

    Added drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,12 @@ class Body extends StatelessWidget {
                   textStyle: const TextStyle(fontSize: 20),
                   backgroundColor: Colors.lightGreen,
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                },
                 child: const Text("LOGIN"),
               ),
             ),
@@ -104,6 +109,53 @@ class Background extends StatelessWidget {
         children: <Widget>[
           child,
         ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Diet Master')),
+      body: Center(child: Text('My Page!')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Diet Master'),
+            ),
+            ListTile(
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('My Kitchen'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Calendar'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
