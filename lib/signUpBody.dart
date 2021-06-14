d89e748c3dd03615ef1bdbc31834791203285347
yWhire <yinan2001@hotmail.com>
2021-06-14 14:12:11 +0300

    update

--- lib/signUpBody.dart
+++ lib/signUpBody.dart
@@ -1,10 +1,9 @@
-import 'package:app/background.dart';
 import 'package:flutter/material.dart';
 
 class SignUpBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Background(
+    return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
